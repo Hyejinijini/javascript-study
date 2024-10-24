1b81a75716e01c614adf7c5f447e2f19c9fe1438
HyeJin <155633287+Hyejinijini@users.noreply.github.com>
2024-10-24 20:46:01 +0900

    변수 선언 및 할당하기
    - let 키워드를 사용해서 변수를 선언
    - 변수명을 참조하여 값을 할당
    - 다른 변수에 변수명을 참조해서 값 복사 가능

---
+++ script/var_let_const.js
@@ -0,0 +1,7 @@
+let admin; // admin 변수 생성(선언)
+let name; // name 변수 생성(선언)
+
+name = "John"; // name 변수에 John 이라는 값을 할당
+admin = name; // admin 변수에 name 변수의 값을 복사
+
+alert(admin); // admin 값을 경고창에 띄우기
